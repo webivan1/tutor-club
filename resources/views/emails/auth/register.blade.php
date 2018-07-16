957f5ebba013e08e51df99b43cbedc7e20629405
Ivan <info-babki@yandex.ru>
2018-07-17 00:33:23 +0300

    test mail

--- resources/views/emails/auth/register.blade.php
+++ resources/views/emails/auth/register.blade.php
@@ -3,8 +3,11 @@
 @section('content')
     @parent
 
+    <?php dump($user, $user->getOriginPassword(), $user->verify_token, $user->isWait()) ?>
+
     {{ t('mail.hello', ['username' => $user->name]) }}
     {{ t('mail.register_info', ['pass' => $user->getOriginPassword()]) }}
+
     @if ($user->isWait())
         <a href="{{ route('verify', ['token' => $user->verify_token]) }}">
             {{ t('mail.activation_link') }}
